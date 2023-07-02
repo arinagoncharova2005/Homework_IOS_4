6f4ae0102ac06278a9a4f3e14e3b9684cc8befc6
a.goncharova <a.goncharova@innopolis.university>
2023-07-02 12:40:13 +0300

    Initial Commit

---
+++ hw4_new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw4_new
+//
+//  Created by Arina Goncharova on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
